0c87fe7ab0ac96f3696f0233742bbdc3573ca70a
Davi <davijuvino@gmail.com>
2022-06-22 12:38:28 -0300

    commit init

---
+++ src/test/java/br/com/rdp/RdpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
